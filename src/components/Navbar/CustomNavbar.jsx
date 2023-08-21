8b19916658a905f67daf6fe16b79f067092075c7
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-08-21 13:24:31 +0200

    navbar styling

--- src/components/Navbar/CustomNavbar.jsx
+++ src/components/Navbar/CustomNavbar.jsx
@@ -1,17 +1,27 @@
+import "../../styles/navbar.css";
 import { NavLink } from "react-router-dom"
 
 const CustomNavbar = () => {
     return (
-        <nav>,
-            <ul>
-                <li><NavLink to="/Translator">Translator</NavLink>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <NavLink to="/Profile">Profile</NavLink>
-                </li>
-            </ul>
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+                Sign Language Translator
+            </a>   <div
+                className="navigation-menu">
+                <ul>
+                    <li><NavLink to="/Translator">Translator</NavLink>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <NavLink to="/Profile">Profile</NavLink>
+                    </li>
+                </ul><ul>
+                    <li>
+                        <NavLink to="/Logout">Logout</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
