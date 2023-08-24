078a4664bd5d94326c607f49059e93e90d926e65
Ritwaan <140506214+Ritwaan@users.noreply.github.com>
2023-08-24 17:22:44 +0200

    jeg har stylet translate siden og lagt til inputboks

--- src/views/TranslationPage.jsx
+++ src/views/TranslationPage.jsx
@@ -4,7 +4,6 @@ import withAuth from '../hoc/withAuth';
 const TranslationPage = () => {
     return (
         <div className="translator-container">
-            <h1>Write text to be translated</h1>
             <Translate />
         </div>
     );
